0a039b02e0392930f3177b035f3eb874475d1dcb
dharmikvarsani <dharmikvarsani968@gmail.com>
2024-12-03 08:17:26 +0530

    javascript Practical And To-Do List

--- JavaScript/To-Do List/todo.js
+++ JavaScript/To-Do List/todo.js
@@ -1,17 +1,52 @@
-let input = document.getElementById("inputtext")
-let task = document.querySelector(".alltask")
+const inputTask = document.getElementById("inputtask")
+const addButton = document.getElementById("addbtn")
+const taskList = document.getElementById("tasklist")
 
-function add(){
-    if (input.value == ""){
-        alert ("Please Enter A Task")
-    }else {
-        let newelement = document.createElement("ul");
-        newelement.innerHTML= `${input.value} <i class="fa-solid fa-trash"></i>`;
-        task.appendChild(newelement);
-        input.value="";
-        newelement.querySelector("i").addEventListener("click" , remove)
-        function remove(){
-            newelement.remove()
-        }
+
+function addTask() {
+
+    const task = inputTask.value.trim();
+
+    if (inputTask.value == "") {
+        alert("Please Enter A Task");
+    } else {
+        createElement(task);
+        inputTask.value = ""
+        saveTask()
     }
 }
+
+addButton.addEventListener("click", addTask)
+
+
+
+function createElement(task) {
+
+    const listItem = document.createElement("li")
+    listItem.innerHTML = task
+    taskList.appendChild(listItem)
+
+    const deleteTask = document.createElement("span")
+    deleteTask.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    listItem.appendChild(deleteTask)
+
+    deleteTask.addEventListener("click", () => {
+        listItem.remove()
+        saveTask()
+    })
+}
+
+
+function saveTask() {
+    let tasks = []
+    taskList.querySelectorAll("li").forEach(function (item) {
+        tasks.push(item.textContent.trim())
+    })
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+function getTask() {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.forEach(createElement)
+}
+getTask()
