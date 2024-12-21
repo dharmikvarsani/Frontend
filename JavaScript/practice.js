cffbd6a84c1cdfee73024573db955b83d347e86a
dharmikvarsani <dharmikvarsani968@gmail.com>
2024-12-21 08:35:58 +0530

    JavaScript Practical

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -1,3 +1,19 @@
-function myfunction(){
-document.getElementById("demo").innerHTML="Hello!!!!!"}
+function myfunction() {
+    document.getElementById("demo").innerHTML = "Hello!!!!!"
+}
 
+
+let a = 0
+
+function count() {
+    a++;
+    postMessage(a)
+    setTimeout("count()", 100)
+}
+count()
+
+onmessage = () => {
+    let i = 0
+    for (i = 0; i <= 10000000000; i++)
+        postMessage(i)
+}
